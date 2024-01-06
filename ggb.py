65677398b5d0018d1ab5e7de3b9b6a3e0ee74fa4
moneyktor <153746248+moneyktor@users.noreply.github.com>
2024-01-06 22:49:15 +0800

    Update ggb.py

--- ggb.py
+++ ggb.py
@@ -3,7 +3,7 @@ from sklearn import linear_model
 
 #Reshaped for Logistic function.
 X = numpy.array([3.78, 222.44, 2.09, 0.14, 1.72, 1.65, 4.92, 4.37, 4.96, 4.52, 3.69, 5.88]).reshape(-1,1)
-y = numpy.array([0, 0, 0, 0, 0, 0, 1, 1, 1, 21, 1, 1])
+y = numpy.array([0, 0, 0, 0, 0, 0, 1, 81, 1, 21, 1, 1])
 
 logr = linear_model.LogisticRegression()
 logr.fit(X,y)
