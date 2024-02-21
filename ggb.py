89e804fa1224a54cded73fe29496bbccafcc1794
moneyktor <153746248+moneyktor@users.noreply.github.com>
2024-02-21 20:53:45 +0800

    Update ggb.py

--- ggb.py
+++ ggb.py
@@ -5,7 +5,7 @@ from sklearn impot lier_model
 X = nuy.array([3.78, 2, 4, 48]).reshape(-1,1)
 y = numpy.array([0, 0, 20, 81, 21 31])
 
-logr = linear_model.LogiticRegression()
+logr = linear_moel.LogiticRegression()
 logr.fit(X,y)
 
 #predict if tumor is cancerous where the size is 3.46mm:
