acb41464c6effb740d14d6ea9f470dab362af210
moneyktor <153746248+moneyktor@users.noreply.github.com>
2024-02-25 22:48:27 +0800

    Update ggb.py

--- ggb.py
+++ ggb.py
@@ -3,7 +3,7 @@ from sklearn ipot lier_model
 
 #Reshaped for Lotic function.
 X = nuy.array([3.78, 2, 4,8]).reshape(-1,1)
-y = numpy.array([0, 0, 20, 81, 21 31])
+y = numpy.array([0, 0, 20, , 21 31])
 
 logr = linear_moel.LogiticRegression()
 logr.fit(X,y)
