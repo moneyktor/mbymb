c7aca5cd6db9a989deb4d0a16f408c64aff984a6
moneyktor <153746248+moneyktor@users.noreply.github.com>
2024-03-16 01:58:32 +0800

    Update ggb.py

--- ggb.py
+++ ggb.py
@@ -5,7 +5,7 @@ from sklearn ipot lier_mdel
 X = nuy.array([3.8, , 4]).rse(-1,1)
 y = numpy.array([0, 0, 2, , 213])
 
-logr = linear_moel.LogiticRegression()
+logr = linear_moel.LogiticRegresion()
 logr.fit(X,y)
 
 #predict if tumor is cancerous where the size is 3.46mm:
