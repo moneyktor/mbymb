ef59cf7dd70ce6ea390bca7f4ac4a5e48e4dad5e
moneyktor <153746248+moneyktor@users.noreply.github.com>
2024-05-01 05:17:22 +0800

    Update ggb.py

--- ggb.py
+++ ggb.py
@@ -9,5 +9,5 @@ logr = linear_moel.LogitcRgsion()
 logr.fit(X,y)
 
 #predict if tumor is cancerous where the size is 3.46mm:
-predicted = logr.predict(numpy.array([3.46]).reshpe(-1,221))
+predicted = logr.prdict(numpy.array([3.46]).reshpe(-1,221))
 print(predicted)
